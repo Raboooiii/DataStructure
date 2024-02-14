199fadb397dac33600642cf31d45b91958f156cf
Raboooiii <146197313+Raboooiii@users.noreply.github.com>
2024-02-15 04:05:04 +0530

    ArrayNotes.h

--- Array/ArrayNotes.h
+++ Array/ArrayNotes.h
@@ -20,7 +20,23 @@ class Array
 		void selection_sort();
 		T binary_search(T);
 		void bubble_sort();
+		template<typename V>
+		void quickSort(V arr[], int low, int high);
+		template<typename W>
+		void mergeSort(W arr[], int l, int r);
 		template<class U>
 		friend ostream & operator <<(ostream &,Array <U>);
 };
-
+template <typename X>
+class Array2 
+{
+	public:
+		 X arr2[100];
+		
+		 void rotate_clockwise(int LB, int UB);
+		 void rotate_anti_clockwise(int LB, int UB);
+		 void listDistinctElement();
+	         void frequencyTable();
+		 template<typename Y>
+		 friend ostream& operator<<(ostream& out, const Array2<Y>& N);
+};
